12cfe15ca9cd38c6418a9ff821ea49fb18660e8c
mirei83 <michael@flowbit.de>
2018-06-08 17:51:46 +0200

    make it pretty

--- Deploy_Testnet.sh
+++ Deploy_Testnet.sh
@@ -13,13 +13,20 @@ apt-get -y install build-essential libgflags-dev libsnappy-dev zlib1g-dev libbz2
 
 
 ### Install go
+echo "########################"
+echo "Installing go"
+echo "########################"
 cd ~
 wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
 chmod +x /usr/local/go/bin/go
+rm go1.10.3.linux-amd64.tar.gz
 
 
 ### Install dep
+echo "########################"
+echo "Installing dep"
+echo "########################"
 cd /usr/local/bin/
 wget https://github.com/golang/dep/releases/download/v0.3.2/dep-linux-amd64
 ln -s dep-linux-amd64 dep
@@ -27,12 +34,16 @@ chmod +x /usr/local/bin/*
 
 
 ### Install VeChain
+echo "Installing VeChain"
 git clone https://github.com/vechain/thor.git $GOPATH/src/VeChain/thor
 cd $GOPATH/src/VeChain/thor
 dep ensure
 make all
 
 ### Create StartUp-Scrip
+echo "########################"
+echo "Creating Startup Script"
+echo "########################"
 cd ~
 echo '#!/bin/bash' >> ./vechain-testnet.sh
 echo "cd"  >> ./vechain-testnet.sh
